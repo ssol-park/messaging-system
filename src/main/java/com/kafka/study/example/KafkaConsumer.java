2cfe56913cf7377df883955f1172c893eca104fa
ssol-park <ksttlov.naver@hanmail.net>
2024-06-29 20:40:02 +0900

    reactor: Remove unused code

--- src/main/java/com/kafka/study/example/KafkaConsumer.java
+++ src/main/java/com/kafka/study/example/KafkaConsumer.java
@@ -2,6 +2,8 @@ package com.kafka.study.example;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.support.KafkaHeaders;
+import org.springframework.messaging.handler.annotation.Header;
 import org.springframework.stereotype.Service;
 
 @Slf4j
@@ -9,7 +11,9 @@ import org.springframework.stereotype.Service;
 public class KafkaConsumer {
 
     @KafkaListener(topics = "${kafka.topic}", groupId = "${kafka.group-id}")
-    public void listen(String message) {
-        log.info("Received message: {}", message);
+    public void listen(String message,
+                       @Header(KafkaHeaders.RECEIVED_PARTITION)  int partition,
+                       @Header(KafkaHeaders.OFFSET)  int offset) {
+        log.info("Received message: {}, partition: {}, offset: {}", message, partition, offset);
     }
 }
