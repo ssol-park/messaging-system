2bea4df630b3117d49e5be81e6191ac7f7b1b951
ssol-park <ksttlov.naver@hanmail.net>
2024-07-02 19:23:18 +0900

    feat: Add Consumer UUID

--- src/main/java/com/kafka/study/example/KafkaConsumer.java
+++ src/main/java/com/kafka/study/example/KafkaConsumer.java
@@ -6,14 +6,20 @@ import org.springframework.kafka.support.KafkaHeaders;
 import org.springframework.messaging.handler.annotation.Header;
 import org.springframework.stereotype.Service;
 
+import java.util.UUID;
+
 @Slf4j
 @Service
 public class KafkaConsumer {
 
+    private final String instanceId = UUID.randomUUID().toString();
     @KafkaListener(topics = "${kafka.topic.name}", groupId = "${kafka.group-id}")
     public void listen(String message,
                        @Header(KafkaHeaders.RECEIVED_PARTITION)  int partition,
                        @Header(KafkaHeaders.OFFSET)  int offset) {
-        log.info("Received message: {}, partition: {}, offset: {}", message, partition, offset);
+        log.info("instanceId:{}, Received message: {}, partition: {}, offset: {}", instanceId, message, partition, offset);
     }
 }
+
+
+
