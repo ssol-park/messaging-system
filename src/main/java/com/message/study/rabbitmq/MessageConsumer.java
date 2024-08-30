401d98e779a5c847391f54a3d034a4c381f0dc50
ssol-park <ksttlov.naver@hanmail.net>
2024-08-30 22:57:56 +0900

    chore: MessageConsumer, SLF4J 로깅으로 변경

--- src/main/java/com/message/study/rabbitmq/MessageConsumer.java
+++ src/main/java/com/message/study/rabbitmq/MessageConsumer.java
@@ -1,13 +1,15 @@
 package com.message.study.rabbitmq;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.stereotype.Component;
 
+@Slf4j
 @Component
 public class MessageConsumer {
 
     @RabbitListener(queues = "myQueue")
     public void receiveMessage(String message) {
-        System.out.println("Received message: " + message);
+        log.info("Received message: {}", message);
     }
 }
