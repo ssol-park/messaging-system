bad0eb22853ce9fa3ab6720db3156c521ba3d46f
ssol-park <ksttlov.naver@hanmail.net>
2024-09-01 09:06:22 +0900

    chore: Producer, Consumer Test 수정
     - 큐 추가로 인해 기존 코드 제거 후 각각 큐 테스트 코드 작성 중

--- src/test/java/com/message/study/rabbitmq/MessageConsumerTest.java
+++ src/test/java/com/message/study/rabbitmq/MessageConsumerTest.java
@@ -15,30 +15,4 @@ import static org.mockito.Mockito.verify;
 @Slf4j
 @SpringBootTest
 class MessageConsumerTest {
-
-    @Autowired
-    private RabbitTemplate rabbitTemplate;
-
-    @SpyBean
-    private MessageConsumer messageConsumer;
-
-    @Test
-    void testReceiveMessage() throws InterruptedException {
-        // given
-        String message = "send message";
-
-        // when: 메세지 전송
-        rabbitTemplate.convertAndSend("myQueue", message);
-
-        Thread.sleep(1000);
-
-        // then: 메시지 수신 여부 확인
-        ArgumentCaptor<String> messageCaptor = ArgumentCaptor.forClass(String.class);
-        verify(messageConsumer).receiveMessage(messageCaptor.capture());
-//        log.info("messageCaptor :: {}", messageCaptor.getValue());
-
-        // AssertJ를 사용하여 메시지 검증
-        assertThat(messageCaptor.getValue()).isNotNull()
-                .isEqualTo(message);
-    }
 }
