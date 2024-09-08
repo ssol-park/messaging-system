4623dbf1a646595ea58c69ef4e7702b5f59e43a1
ssol-park <ksttlov.naver@hanmail.net>
2024-09-08 15:10:34 +0900

    refactor: RabbitMQProperties, 롬복 추가

--- rabbitmq/src/main/java/com/rabbitmq/config/RabbitMQProperties.java
+++ rabbitmq/src/main/java/com/rabbitmq/config/RabbitMQProperties.java
@@ -5,20 +5,19 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 
 @Getter
+@RequiredArgsConstructor
 @ConfigurationProperties(prefix = "spring.rabbitmq")
 public class RabbitMQProperties {
+
+    private final String host;
+    private final int port;
+    private final String username;
+    private final String password;
 
     private final Direct direct;
     private final Topic topic;
     private final Fanout fanout;
     private final Headers headers;
-
-    public RabbitMQProperties(Direct direct, Topic topic, Fanout fanout, Headers headers) {
-        this.direct = direct;
-        this.topic = topic;
-        this.fanout = fanout;
-        this.headers = headers;
-    }
 
     @Getter
     @RequiredArgsConstructor
